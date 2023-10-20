f0c5e554d9dbe9d13b58b2d785b5b04e6f148b0c
Khanh Nguyen <Kn11072003a@gmail.com>
2023-10-20 20:48:12 +0700

    change rule in eslint

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // const express = require('express');
 // const path = require('path');
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
 
 const app = express();
 const PORT = 3000;
 const __dirname = path.resolve(); // return the current working directory
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Start the server
 app.listen(PORT, () => {
@@ -16,6 +16,6 @@ app.listen(PORT, () => {
 });
 
 
-app.get('/', (req, res) => {
-    res.render('index.html');
+app.get("/", (req, res) => {
+    res.render("index.html");
 });
