458d6dbb09decd5489ec76ed1b23dbfe7f2df5dd
Khanh Nguyen <Kn11072003a@gmail.com>
2023-10-20 20:43:31 +0700

    Setup prettier and eslint

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const path = require('path');
+// const express = require('express');
+// const path = require('path');
+import express from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = 3000;
+const __dirname = path.resolve(); // return the current working directory
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
+
 
 app.get('/', (req, res) => {
     res.render('index.html');
 });
-
