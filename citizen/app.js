d95f5d977875c5863e3b9a1e7f455762644ea850
Gia Thịnh <trangiathinh53@gmail.com>
2023-10-23 22:44:58 +0700

    folder structure

---
+++ citizen/app.js
@@ -0,0 +1,21 @@
+// const express = require('express');
+// const path = require('path');
+import express from 'express'
+import path from 'path'
+
+const app = express()
+const PORT = 4000
+const __dirname = path.resolve() // return the current working directory
+
+app.use(express.static(path.join(__dirname, 'public')))
+// EJS
+app.set('view engine', 'ejs')
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`)
+})
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
