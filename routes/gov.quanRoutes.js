a47cd3422246430441ef2b98bca27b47330b6846
Gia Thịnh <trangiathinh53@gmail.com>
2023-11-09 22:19:19 +0700

    Chi tiet bang QC + yeu cau chinh sua bang

--- routes/gov.quanRoutes.js
+++ routes/gov.quanRoutes.js
@@ -13,12 +13,28 @@ router.get('/diemdat', (req, res) => {
   res.render('quan/Diemdat', { title: 'Quận - Điểm đặt' });
 })
 
+router.get('/bangquangcao', (req, res) => {
+  res.render('quan/BangQC', { title: 'Quận - Bảng Quảng cáo' });
+})
+
+router.get('/yeucau/cp', (req, res) => {
+  res.render('quan/YeuCauCP', { title: 'Quận - Yêu cầu cấp phép' });
+})
+
 router.get('/diemdat/:id', (req, res) => {
   res.render('quan/ChitietDiemDat', { title: 'Quận - Chi tiết điểm đặt' });
 })
 
-router.get('/yeucau/cs/:id', (req, res) => {
-  res.render('quan/YeuCauCS', { title: 'Quận - Yêu cầu chỉnh sửa' });
+router.get('/bangquangcao/:id', (req, res) => {
+  res.render('quan/ChitietBangQC', { title: 'Quận - Chi tiết bảng quảng cáo' });
+})
+
+router.get('/yeucau/cs/diemdat/:id', (req, res) => {
+  res.render('quan/YeuCauCS-diem', { title: 'Quận - Yêu cầu chỉnh sửa điểm đặt' });
+})
+
+router.get('/yeucau/cs/bangquangcao/:id', (req, res) => {
+  res.render('quan/YeuCauCS-bang', { title: 'Quận - Yêu cầu chỉnh sửa bảng quảng cáo' });
 })
 
 router.get('/baocao/:id', (req, res) => {
