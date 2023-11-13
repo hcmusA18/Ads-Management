3bb3ee2a8b1b8031f03fb9cde66564cad96e7ce9
Khanh Nguyen <Kn11072003a@gmail.com>
2023-11-13 14:18:03 +0700

    Create government branch

--- app.js
+++ app.js
@@ -8,40 +8,17 @@ import quanRoutes from './routes/gov.quanRoutes.js'
 import phuongRoutes from './routes/gov.phuongRoutes.js'
 import citizenRoutes from './routes/citizenRoutes.js'
 
-const GOV_PORT = 3000
-const CITIZEN_PORT = 4000
+const PORT = process.env.PORT || 3000
 const __dirname = path.resolve() // return the current working directory
-const appCitizen = express()
-const appGov = express()
+const app = express()
 
 
 dotenv.config(path.join(__dirname, '.env'))
-
-// Citizen
-
-// Public folder
-appCitizen.use(express.static(path.join(__dirname, 'citizen/public')))
-
-//Views folder
-// appCitizen.set('views', path.join(__dirname, 'citizen/views'))
-
-// Routes
-appCitizen.use('/', citizenRoutes);
-
-// Start the server
-const serverCitizen = http.createServer(appCitizen);
-serverCitizen.listen(CITIZEN_PORT, () => {
-  console.log(`Citizen server is running at http://localhost:${CITIZEN_PORT}`)
-})
-
-
-
-
 // Government
 
 // Public folder
-appGov.use(express.static(path.join(__dirname, 'government/public')))
-appGov.use((req, res, next) => {
+app.use(express.static(path.join(__dirname, 'public')))
+app.use((req, res, next) => {
   res.locals.url = req.originalUrl
   res.locals.host = req.get('host')
   res.locals.protocol = req.protocol
@@ -49,22 +26,19 @@ appGov.use((req, res, next) => {
 })
 
 //Views folder
-appGov.set('views', path.join(__dirname, 'government/views'))
+app.set('views', path.join(__dirname, 'views'))
 
 // Routes
-// appGov.use('/', govRoutes)
-appGov.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.render('index', { title: 'Cán bộ' })
 })
-appGov.use('/so', soRoutes)
-appGov.use('/quan', quanRoutes)
-appGov.use('/phuong', phuongRoutes)
+app.use('/so', soRoutes)
+app.use('/quan', quanRoutes)
+app.use('/phuong', phuongRoutes)
 
 // EJS
-appGov.set('view engine', 'ejs')
+app.set('view engine', 'ejs')
 
-// Start the server
-const serverGov = http.createServer(appGov);
-serverGov.listen(GOV_PORT, () => {
-  console.log(`Government server is running at http://localhost:${GOV_PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`)
+});
